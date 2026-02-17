10828f703d598862955974367d010ccadf866fd6
Narayandwivedi <desinplus1@gmail.com>
2026-02-17 19:56:19 +0530

    improve ui

--- frontend/src/components/HeroCarousel.jsx
+++ frontend/src/components/HeroCarousel.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 
 const HeroCarousel = () => {
   return (
-    <section className="w-full md:w-[70%] lg:w-[43%] mx-auto px-4 md:px-0">
-      <div className="grid grid-cols-2 gap-3 md:gap-5 items-center">
+    <section className="w-full md:w-[70%] lg:w-[46%] mx-auto px-1 sm:px-4 md:px-0">
+      <div className="grid grid-cols-2 gap-2 md:gap-5 items-center">
         <img
           src="/banana.png"
           alt="Banana Hero Banner"
-          className="w-full lg:w-[80%] h-auto block mx-auto"
+          className="w-full lg:w-[90%] h-auto block mx-auto"
         />
         <img
-          src="/onion.png"
+          src="/apple.png"
           alt="Onion Hero Banner"
-          className="w-full lg:w-[80%] h-auto block mx-auto"
+          className="w-full lg:w-[90%] h-auto block mx-auto"
         />
       </div>
     </section>
